d9d1f3b4726e175e1a31ada45788436b1ef0b3de
Iliyan-Y <ili.yankov91@gmail.com>
2021-04-06 16:39:48 +0100

    Log in when email is verified

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,54 +1,71 @@
-import Firebase, { db } from '../../../config/Firebase.js'
+import Firebase, { db } from '../../../config/Firebase.js';
 
-import { UserActionTypes } from './user.types'
+import { UserActionTypes } from './user.types';
 
-export const updateEmail = email => {
+export const updateEmail = (email) => {
   return {
-      type: UserActionTypes.UPDATE_EMAIL,
-      payload: email
-  }
-}
+    type: UserActionTypes.UPDATE_EMAIL,
+    payload: email,
+  };
+};
 
-export const updatePassword = password => {
+export const updatePassword = (password) => {
   return {
-      type: UserActionTypes.UPDATE_PASSWORD,
-      payload: password
-  }
-}
+    type: UserActionTypes.UPDATE_PASSWORD,
+    payload: password,
+  };
+};
 
 export const logout = () => {
   return {
-      type: UserActionTypes.LOGOUT
-  }
-}
+    type: UserActionTypes.LOGOUT,
+  };
+};
 
 export const login = () => {
   return async (dispatch, getState) => {
-      try {
-          const { email, password } = getState().user
-          const response = await Firebase.auth().signInWithEmailAndPassword(email, password)
-          
-          dispatch(getUser(response.user.uid))
-      } catch (e) {
-          alert(e)
+    try {
+      const { email, password } = getState().user;
+      const response = await Firebase.auth().signInWithEmailAndPassword(
+        email,
+        password
+      );
+      if (response.user) {
+        const user = {
+          uid: response.user.uid,
+          email: response.user.email,
+          emailVerified: response.user.emailVerified,
+        };
+
+        console.log(response.user);
+        dispatch({ type: UserActionTypes.LOGIN, payload: user });
       }
-  }
-}
+      //dispatch(getUser(response.user.uid));
+    } catch (e) {
+      alert('Log In: ' + e);
+    }
+  };
+};
 
-export const getUser = uid => {
-	return async (dispatch, getState) => {
-		try {
-			const user = await db
-				.collection('users')
-				.doc(uid)
-				.get()
+export const getUser = (uid) => {
+  return async (dispatch, getState) => {
+    try {
+      const user = await db.collection('users').doc(uid).get();
 
-			dispatch({ type: UserActionTypes.LOGIN, payload: user.data() })
-		} catch (e) {
-			alert(e)
-		}
-	}
-}
+      //console.log(user.data());
+
+      // const user = {
+      //   uid: user.uid,
+      //   email: user.email,
+      //   emailVerified: user.emailVerified,
+      // };
+
+      dispatch({ type: UserActionTypes.LOGIN, payload: user.data() });
+    } catch (e) {
+      alert('getUser: ' + e);
+    }
+  };
+};
 
 var actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
@@ -57,46 +74,49 @@ var actionCodeSettings = {
   // This must be true.
   handleCodeInApp: true,
   iOS: {
-    bundleId: 'com.example.ios'
+    bundleId: 'com.example.ios',
   },
   android: {
     packageName: 'com.example.android',
     installApp: true,
-    minimumVersion: '12'
+    minimumVersion: '12',
   },
-  dynamicLinkDomain: 'example.page.link'
+  dynamicLinkDomain: 'example.page.link',
 };
 
 export const signup = () => {
   return async (dispatch, getState) => {
-      try {
-          const { email, password } = getState().user
-              const response = await Firebase.auth().createUserWithEmailAndPassword(email, password)
-              
-              if (response.user.uid) {
-                const user = {
-                  uid: response.user.uid,
-                  email: email,
-                  emailVerified: response.user.emailVerified
-                }
-    
-                db.collection('users')
-                  .doc(response.user.uid)
-                  .set(user)
-    
-                dispatch({ type: UserActionTypes.SIGNUP, payload: response.user })
-    
-                response.user.sendEmailVerification()
-                  .then(function() {
-                    console.log('Email sent.');
-                  })
-                  .catch(function(error) {
-                    console.log(`An error happened: ${error}`)
-                });
-              }
-              // ...
-      } catch (e) {
-          alert(e)
+    try {
+      const { email, password } = getState().user;
+      const response = await Firebase.auth().createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      if (response.user.uid) {
+        const user = {
+          uid: response.user.uid,
+          email: email,
+          emailVerified: response.user.emailVerified,
+        };
+
+        db.collection('users').doc(response.user.uid).set(user);
+
+        dispatch({ type: UserActionTypes.SIGNUP, payload: response.user });
+
+        response.user
+          .sendEmailVerification()
+          .then(function () {
+            console.log('Email sent.');
+            alert('Please verify your email');
+          })
+          .catch(function (error) {
+            console.log(`An error happened: ${error}`);
+          });
       }
-  }
-}
+      // ...
+    } catch (e) {
+      alert(e);
+    }
+  };
+};
